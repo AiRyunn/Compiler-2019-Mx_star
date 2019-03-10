6276bdd4db01d0de0a91c5a6be48e595144d93a0
Espylapiza <espylapiza@gmail.com>
2019-03-10 12:48:11 +0800

    input test

--- src/Compile.java
+++ src/Compile.java
@@ -3,8 +3,13 @@ import java.io.*;
 
 public class Compile {
     public static void main(String[] args) throws IOException {
+        try {
             InputStream istream = new FileInputStream("program.txt");
-            
+
             System.out.println("target asm code");
+        } catch (final IOException e) {
+            System.err.println(e);
+            return;
+        }
     }
 }
