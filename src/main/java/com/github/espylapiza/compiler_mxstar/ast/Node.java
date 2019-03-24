7498cc3a6994322708b9a987a5cb10bd400bb3de
Espylapiza <espylapiza@gmail.com>
2019-03-24 19:47:13 +0800

    reconstructure

--- src/main/java/com/github/espylapiza/compiler_mxstar/ast/Node.java
+++ src/main/java/com/github/espylapiza/compiler_mxstar/ast/Node.java
@@ -8,19 +8,46 @@ class PizzaIR extends Node {
 
 }
 
-class Function extends Node {
-    String name, rtype;
+class ParamListInstance extends Node {
+    ParamsInstance params;
 
-    Function(String name, String type) {
-        this.name = name;
-        this.rtype = type;
+    ParamListInstance(ParamsInstance params) {
+        this.params = params;
     }
 }
 
-class ParamListDefinition extends Node {
-    ParamsInstance params;
+class ParamList extends Node {
+    Params params;
 
-    ParamListDefinition(ParamsInstance params) {
+    ParamList(Params params) {
         this.params = params;
+    }
+}
+
+class FunctionNode extends Node {
+    String name, rtype;
+
+    FunctionNode(String name, String rtype) {
+        this.name = name;
+        this.rtype = rtype;
+    }
+}
+
+class VariableNode extends Node {
+    String name, type;
+
+    VariableNode(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+}
+
+class ObjectNode extends Node {
+    String name, type, owner;
+
+    ObjectNode(String name, String type, String owner) {
+        this.name = name;
+        this.type = type;
+        this.owner = owner;
     }
 }
